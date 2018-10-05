890aa0f3441398ce69fb89b4aef22d48ab633444
AmirHosseinifar <hosseinifar.amir@gmail.com>
2018-10-05 13:17:12 -0700

    Add day count page

---
+++ src/components/RunDayCount.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class RunDayCount extends Component {
+  render() {
+    return (
+      <div className="run-day-count">
+        <div className="total-days">
+          <span>5 days</span>
+        </div>
+        <div className="rain-days">
+          <span>2 days</span>
+        </div>
+        <div className="dry-days">
+          <span>3 days</span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RunDayCount

86a56e23185c915fa106c5307b487c0d7d8d138c
AmirHosseinifar <hosseinifar.amir@gmail.com>
2018-10-05 16:36:19 -0700

    Add goal progress calculation

--- src/components/RunDayCount.js
+++ src/components/RunDayCount.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react'
 import '../stylesheets/ui.scss'
 
 class RunDayCount extends Component {
+
+  percentToDecimal(decimal) {
+    return ( (decimal*100) + '%')
+  }
+
+  calcGoalProgress(total, goal) {
+    return this.percentToDecimal(total/goal)
+  }
+
   render() {
     return (
       <div className="run-day-count">
         <div className="total-days">
-          <span>5 days</span>
+          <span>{this.props.total}</span>
+          <span>days</span>
         </div>
         <div className="rain-days">
-          <span>2 days</span>
+          <span>{this.props.rainDay}</span>
+          <span>days</span>
         </div>
         <div className="dry-days">
-          <span>3 days</span>
+          <span>{this.props.dryDay}</span>
+          <span>days</span>
+        </div>
+        <div>
+          <span>{this.calcGoalProgress(this.props.total, 
+                                       this.props.goal)}
+          </span>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import RunDayCount from './components/RunDayCount'
 
-ReactDOM.render(<RunDayCount />, document.getElementById('root'));
+ReactDOM.render(<RunDayCount total ={50}
+                             rainDay={30}
+                             dryDay={10}
+                             goal={100}
+/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

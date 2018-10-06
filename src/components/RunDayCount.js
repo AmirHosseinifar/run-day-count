b7ed7ff7809170e3829690c5cfb80b6c582d8037
AmirHosseinifar <hosseinifar.amir@gmail.com>
2018-10-05 17:39:03 -0700

    Change the react class with the stateless functional components

--- src/components/RunDayCount.js
+++ src/components/RunDayCount.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../stylesheets/ui.scss'
 
-class RunDayCount extends Component {
 
-  percentToDecimal(decimal) {
+
+  const percentToDecimal = (decimal) => {
     return ( (decimal*100) + '%')
   }
 
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
+  const calcGoalProgress = (total, goal) => {
+    return percentToDecimal(total/goal)
   }
 
-  render() {
+  const RunDayCount = ({total, rainDay, dryDay, goal}) => {
     return (
       <div className="run-day-count">
         <div className="total-days">
-          <span>{this.props.total}</span>
+          <span>{total}</span>
           <span>days</span>
         </div>
         <div className="rain-days">
-          <span>{this.props.rainDay}</span>
+          <span>{rainDay}</span>
           <span>days</span>
         </div>
         <div className="dry-days">
-          <span>{this.props.dryDay}</span>
+          <span>{dryDay}</span>
           <span>days</span>
         </div>
         <div>
-          <span>{this.calcGoalProgress(this.props.total, 
-                                       this.props.goal)}
+          <span>{calcGoalProgress(total, goal)}
           </span>
         </div>
       </div>
     )
   }
-}
+
 
 export default RunDayCount

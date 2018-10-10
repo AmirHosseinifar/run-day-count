417f95e87a5e152917f3e8b928e6c9ca7c8e40d5
AmirHosseinifar <hosseinifar.amir@gmail.com>
2018-10-09 17:35:54 -0700

    Add react router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import RunDayCount from './components/RunDayCount';
 import {App} from './components/App'
+import {HashRouter, Route} from 'react-router-dom'
+import {Whoops404} from './components/Whoops404'
 
 ReactDOM.render(
-  <App />
+  <HashRouter>
+    <div>
+      <Route exact path="/" component={App}/>
+      <Route path="*" component={Whoops404}/>
+    </div>
+  </HashRouter>
+  
   ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

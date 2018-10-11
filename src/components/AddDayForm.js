4a2b75a028538c064be872601e9df4943c3b7d67
AmirHosseinifar <hosseinifar.amir@gmail.com>
2018-10-10 19:13:22 -0700

    Add button for submitting the form

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -3,25 +3,53 @@ import {Component} from 'react'
 import {PropTypes} from 'prop-types'
 
 export class AddDayForm extends Component {
-  render() {
 
+  constructor(props) {
+    super(props)
+    this.submit = this.submit.bind(this)
+  }
+  
+  submit(e) {
+    e.preventDefault()
+    console.log('location', this.refs.location.value)
+    console.log('date', this.refs.date.value)
+    console.log('rainDay', this.refs.rainDay.checked)
+    console.log('dryDay', this.refs.dryDay.checked)
+  }
+  
+  render() {
     const {location, date, rainDay, dryDay} = this.props
     return (
-      <form className="add-day-form">
+      <form onSubmit={this.submit} className="add-day-form">
         <label htmlFor="location">Location</label>
-        <input id="location" type="text" required defaultValue={location}/>
+        <input id="location" 
+               type="text" 
+               required 
+               defaultValue={location}
+               ref="location"/>
 
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" required defaultValue={date}/>
+        <input id="date" 
+               type="date" 
+               required 
+               defaultValue={date}
+               ref="date"/>
 
         <div>
-          <input id="rainDay" type="checkbox" defaultChecked={rainDay}/>
+          <input id="rainDay" 
+                 type="checkbox" 
+                 defaultChecked={rainDay}
+                 ref="rainDay"/>
           <label htmlFor="rainDay">Rainy Day</label>
         </div>
         <div>
-          <input id="dryDay" type="checkbox" defaultChecked={dryDay}/>
+          <input id="dryDay" 
+                 type="checkbox" 
+                 defaultChecked={dryDay}
+                 ref="dryDay"/>
           <label htmlFor="dryDay">Dry Day</label>
         </div>
+        <button>Add Day</button>
       </form>
     )
   }
